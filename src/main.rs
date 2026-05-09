dc027b968daf2ff1ad308b6ed603170aabff0b05
austinjohnson03 <amj102998@gmail.com>
2026-05-08 23:28:16 -0400

    added basic health check to main
    Signed-off-by: austinjohnson03 <amj102998@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use ss_db::client::{Client, Config};
+
+#[tokio::main]
+async fn main() {
+    let db_client = Client::new(&Config::from_env()).await;
+    db_client.ping().await;
 }
